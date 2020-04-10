8deb7305ce2ca5269ebdce888d6aa0c882be2440
Chandra Annamaneni <chandra627@gmail.com>
2020-04-10 08:21:56 +0100

    Changes to minimize flutter

--- kernel/fan_control.c
+++ kernel/fan_control.c
@@ -29,21 +29,11 @@ static int sensor_error = 538;
 module_param(sensor_error, uint, 0644);
 MODULE_PARM_DESC(sensor_error, "Sensor error");
 
-void set_fan_half_speed(void)
-{
-    pstate.duty_cycle = default_max_duty_cycle/2;
-    if (pwm_apply_state(pdevice, &pstate))
-        pr_info("Error setting pwm state\n");
-    pr_info("set fan to half max speed\n");
-}
-
-void set_fan_speed(int dc)
-{
-    pstate.duty_cycle = dc;
-    if (pwm_apply_state(pdevice, &pstate))
-        pr_info("Error setting pwm state\n");
-}
-
+/*
+ * init_pwm_control
+ * Initializes the pwm hardware device and gains control. The duty cycle is 
+ * set to 0 and the period to default_max_duty_cycle.
+ */
 static int init_pwm_control(void)
 {
     struct device_node *dev_node;
@@ -80,15 +70,48 @@ static int init_pwm_control(void)
     return 0;
 }
 
+/*
+ * deinit_pwm_control
+ * Deinitializes the pwm hardware and returns control.
+ */
 static void deinit_pwm_control(void)
 {
-    set_fan_speed(0);
     if (pdevice != NULL) {
         pwm_disable(pdevice);
         pwm_put(pdevice);
     }
 }
 
+/*
+ * set_fan_half_speed
+ * Sets the fan to half speed. Used for debugging.
+ */
+void set_fan_half_speed(void)
+{
+    pstate.duty_cycle = default_max_duty_cycle/2;
+    if (pwm_apply_state(pdevice, &pstate))
+        pr_info("Error setting pwm state\n");
+    pr_info("set fan to half max speed\n");
+}
+
+/*
+ * set_fan_speed
+ * Sets the pwm duty cycle and therefore the speed of the fan.
+ * The fraction duty cycle divided by period is the fraction of 
+ * the max speed of the fan.
+ */
+void set_fan_speed(int dc)
+{
+    pstate.duty_cycle = dc;
+    if (pwm_apply_state(pdevice, &pstate))
+        pr_info("Error setting pwm state\n");
+}
+
+/*
+ * get_temperature
+ * Reads and returns the cpu temperature. Reads the temperature thrice
+ * in succession and picks the best value.
+ */
 static int get_temperature(void)
 {
     int temp_array[3];
@@ -116,12 +139,28 @@ static int get_temperature(void)
 
     return temp;
 }
+/*
+ * Raspberry pi's temperature sensor has a large error of 
+ * +/- 2 * 538 millicentigrade. This error manifests itself intermittently 
+ * even at steady state at no load and no cooling. This is why we read
+ * the temperature thrice in get_temperature and there is an adjustment
+ * to the error in pid_control. However even with this adjustments, 
+ * there is a degree of flutter in the fan speed when the pid control is 
+ * in operation. The two adjustments merely reduce the magnitude of 
+ * the flutter
+ */
 
-static void adjust_fan_speed(void)
+/*
+ * pid_control 
+ * Calculates the appropriate fan speed based on the difference between
+ * the current temperature and the set point.
+ */
+static void pid_control(void)
 {
     static int last_error = 0;
     static int error_sum = 0;
     static int total_time = 0;
+    static int prev_dc;
     int current_temp;
     int error = 0;
     int current_dc;
@@ -129,6 +168,8 @@ static void adjust_fan_speed(void)
 
     if (set_point == 0)
         return;
+
+    total_time += period;
 
     if ((current_temp = get_temperature()) == 0)
         return;
@@ -139,34 +180,46 @@ static void adjust_fan_speed(void)
     }
     
     error = current_temp - set_point;
+    error_sum += error;
     if (error <= 0)
         return;
 
     if (abs(error) < nominal_error)
         error = 0;
 
-    error_sum += error;
-    total_time += period;
     current_dc = prop_constant * (error + 
                                  (error_sum / total_time) + 
                                  period * (error - last_error));
 
     if (current_dc > default_max_duty_cycle)
         current_dc = default_max_duty_cycle;
-    
-    set_fan_speed(current_dc);
-    pr_info("fan speed is %d\n", current_dc);
+ 
+    if (abs(current_dc - prev_dc) > (5 * default_max_duty_cycle)/100)   
+        set_fan_speed(current_dc);
 
+    prev_dc = current_dc;
     last_error = error;
+
+    pr_info("fan speed is %d\n", current_dc);
 }
+
+/*
+ * timer_func
+ * @exp_timer: the timer struct.
+ * Adds a timer to the list of kernel timers. When called, updates the 
+ * expiry time.
+ */
 
 static void timer_func(struct timer_list *exp_timer)
 {
-    adjust_fan_speed();
+    pid_control();
     exp_timer->expires += period * HZ;
     add_timer(exp_timer);
 }
 
+/* init_pid_timer
+ * Initialzes the timer.
+ */
 static void init_pid_timer(void)
 {
     pid_timer.expires = jiffies + period * HZ;
@@ -174,7 +227,10 @@ static void init_pid_timer(void)
     add_timer(&pid_timer);   
 }
 
-
+/*
+ * init_fan_control
+ * Module entry point.
+ */
 static int init_fan_control(void)
 {
     thermal_zone = thermal_zone_get_zone_by_name("cpu-thermal");
@@ -193,6 +249,10 @@ static int init_fan_control(void)
     return 0;
 }
 
+/*
+ * exit_fan_control
+ * Module exit point.
+ */
 static void exit_fan_control(void)
 {
     del_timer(&pid_timer);
